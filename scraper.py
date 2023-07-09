45364caa4ec50f2cbfc0f345aacfc582bf22bf6c
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-09 21:49:46 +0200

    Add script to get all products from x-kom.pl in all categories

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -6,59 +7,67 @@ from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-class ScraperConfig:
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+
+def get_ids():
+    with open("x_kom_categories.json", 'r', encoding="utf-8") as file:
+        data = json.load(file)
+
+    ids = []
+
+    def extract_ids(data):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key == 'Id':
+                    ids.append(value)
+                if isinstance(value, (dict, list)):
+                    extract_ids(value)
+        elif isinstance(data, list):
+            for item in data:
+                extract_ids(item)
+
+    extract_ids(data)
+
+    return ids
+
+
+def return_all_urls():
     """
-    Selenium scraper configuration class.
+    Return all urls for every category
     """
-    def __init__(self, cookie_button, url):
-        self.chrome_options = Options()
-        self.chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
-        self.cookie_button = cookie_button
-        self.url = url
+    urls = []
+    ids = get_ids()
 
-    def get_driver(self) -> None:
-        """
-        Method to get the driver for selenium
-        """
-        self.driver.get(self.url)
+    for id in ids:
+        urls.append(f"https://www.x-kom.pl/g-2/c/{id}.html")
 
-    def quit_driver(self) -> None:
-        """
-        Method to quit the driver for selenium
-        """
-        self.driver.quit()
+    return urls
 
-    def accept_cookies(self) -> None:
-        """
-        Method to accept cookies
-        """
+
+counter = 0
+for url in return_all_urls():
+
+    driver.get(url)
+
+    if counter == 0:
         try:
-            cookie_button = self.driver.find_element(By.CSS_SELECTOR, self.cookie_button)
-            ActionChains(self.driver).move_to_element(cookie_button).click(cookie_button).perform()
+            cookie_button = driver.find_element(By.CSS_SELECTOR, ".sc-15ih3hi-0.sc-1p1bjrl-9.dRLEBj")
+            ActionChains(driver).move_to_element(cookie_button).click(cookie_button).perform()
         except Exception as e:
-            print(f"Couldn't accept cookies: {str(e)}")
-
-
-URL = 'https://www.x-kom.pl/g-2/c/161-akcesoria-komputerowe.html'
-config = ScraperConfig(cookie_button=".sc-15ih3hi-0.sc-1p1bjrl-9.dRLEBj", url=URL)
-config.get_driver()
-config.accept_cookies()
-
-try:
-    # Get the number of pages
-    pages = config.driver.find_elements(By.CSS_SELECTOR, 'a.sc-1h16fat-0.sc-1xy3kzh-0.gPKgJT')
-    max_page = max([int(page.get_attribute('href').split('=')[-1]) for page in pages])
-
-    for page_number in range(1, max_page+1):
-        config.driver.get(f"https://www.x-kom.pl/g-2/c/161-akcesoria-komputerowe.html?page={page_number}")
-        prices = config.driver.find_elements(By.CSS_SELECTOR, 'span[data-name="productPrice"]')
-        product_names = config.driver.find_elements(By.CSS_SELECTOR, 'h3.sc-16zrtke-0.kGLNun.sc-1yu46qn-9.feSnpB')
+            print(f"Nie udało się zamknąć modala cookies: {str(e)}")
+    counter += 1
+    try:
+        prices = driver.find_elements(By.CSS_SELECTOR, 'span[data-name="productPrice"]')
+        product_names = driver.find_elements(By.CSS_SELECTOR, 'h3.sc-16zrtke-0.kGLNun.sc-1yu46qn-9.feSnpB')
 
         for price, name in zip(prices, product_names):
-            print(f"Product: {name.text}, Price: {price.text}")
+            print(f"Produkt: {name.text}, Cena: {price.text}")
 
-except Exception as e:
-    print(f"There was an error: {str(e)}")
+    except Exception as e:
+        print(f"Nie udało się pobrać informacji o produktach: {str(e)}")
 
-config.quit_driver()
+driver.quit()
