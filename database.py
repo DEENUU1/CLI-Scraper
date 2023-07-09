8cafdea885deb170df24018069d106200bce0cd1
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-09 23:09:31 +0200

    Add Website and Product model schema (#1)

--- database.py
+++ database.py
@@ -1,14 +1,16 @@
+from datetime import datetime
 from typing import Optional, List
 from pydantic import BaseModel
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import Session, sessionmaker, relationship
 
-
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
 
 def get_db():
@@ -17,3 +19,27 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+class Website(Base):
+    __tablename__ = "websites"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    objects_number = Column(Integer, default=0)
+    last_update = Column(DateTime, default=datetime.utcnow)
+
+    products = relationship("Product", back_populates="website")
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    website_id = Column(Integer, ForeignKey("websites.id"))
+    date_created = Column(DateTime, default=datetime.utcnow)
+    date_updated = Column(DateTime, default=datetime.utcnow)
+
+    website = relationship("Website", back_populates="products")
